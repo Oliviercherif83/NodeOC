74d4faedd5aaaf26740f0551c763995679307c1e
ocherif <o.cherif@my-groom-service.com>
2021-12-16 14:48:37 +0100

    vueTabStudents

--- Module4-Server02/Exercice-revision/server.js
+++ Module4-Server02/Exercice-revision/server.js
@@ -1,17 +1,18 @@
 const http = require("http");
 const fs = require("fs");
-
 const hostname = "127.0.0.1";
 const port = 8080;
-
+const {students} = require("./Data/students")
 // tableau d'étudiant
+/*
 const students = [
   { name: "Sonia", birth: "2019-14-05" },
   { name: "Antoine", birth: "2000-12-05" },
   { name: "Alice", birth: "1990-14-09" },
   { name: "Sophie", birth: "2001-10-02" },
   { name: "Bernard", birth: "1980-21-08" },
-];
+];*/
+
 const server = (req, res) => {
   const url = req.url.replace("/", "");
   // pas de favicon pour l'instant
@@ -29,10 +30,11 @@ const server = (req, res) => {
     return;
   }
 
-  if (url === "") {
+  if (url === "students") {
+    //const students = fs.readFileSync("../Data/students.js", "utf-8");
     let html = "<ul>";
-    for (const { name } of students) {
-      html += `<li>${name}</li>`;
+    for (const { name, birth } of students) {
+      html += `<li>étudiant ${name}, née le ${birth}</li>`;
     }
     html += "</ul>";
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -44,19 +46,31 @@ const server = (req, res) => {
         <title>Ajoutez un utilisateur</title>
         <link href="/bootstrap" rel="stylesheet" type="text/css" />
     </head>
-    
     <body>
         <div class="container">
             <div class="row">
                 ${html}
             </div>
         </div>
-    
     </body>`);
     res.end();
     return;
   }
+
+  if (url === "home") {
+    const home = fs.readFileSync("./views/home.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(home);
+    res.end();
+    return;
+  }
+
+
+  if(url === "" && req.method === 'POST'){
+    
+  }
 };
+
 const app = http.createServer(server);
 
 app.listen(port, hostname, () => {
